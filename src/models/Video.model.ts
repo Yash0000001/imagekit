25119172be6eab0821286bbd7da42ae11d191f46
Yash0000001 <yashqwe58@gmail.com>
2025-06-24 15:29:56 +0530

    feat:user and video models created

---
+++ src/models/Video.model.ts
@@ -0,0 +1,44 @@
+import mongoose, { Schema, model, models } from "mongoose";
+
+export const Video_Dimensions = {
+    width: 1080,
+    height: 1920,
+} as const;
+
+export interface IVideo {
+    _id?: mongoose.Types.ObjectId;
+    title: string;
+    description: string;
+    videoUrl: string;
+    thumbnailUrl: string;
+    controls?: boolean;
+    transformations?: {
+        height: number;
+        width: number;
+        quality?: number;
+    };
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+const videoSchema = new Schema<IVideo>(
+    {
+        title: { type: String, required: true, unique: true },
+        description: { type: String, required: true, unique: true },
+        videoUrl: { type: String, required: true },
+        thumbnailUrl: { type: String, required: true },
+        controls: { type: Boolean, required: true },
+        transformations: {
+            height: { type: Number, default: Video_Dimensions.height },
+            width: { type: Number, default: Video_Dimensions.width },
+            quality: { type: Number, min: 1, max: 100 },
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Video = models?.Video || model<IVideo>("Video", videoSchema);
+
+export default Video
